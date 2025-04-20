2a72a6148d03799a8a7cbca98dc6047cbf415d90
dd <the.jesse.dev@gmail.com>
2025-04-19 17:06:50 -0700

    loop done

--- apps/web/app/[id]/_components/interview-session.tsx
+++ apps/web/app/[id]/_components/interview-session.tsx
@@ -3,15 +3,32 @@
 import { Doc } from "@residency/api";
 import { Button } from "@residency/ui/components/button";
 
-// Use the generated Doc type from Convex for a cleaner type definition
 export const InterviewSession = (props: { user: Doc<"users"> }) => {
   const { user } = props;
+
+  if (!user.active) {
+    return (
+      <div className="flex items-center justify-center min-h-svh">
+        <h1>
+          Hi {user.name}! We're working on evaluating your application. You'll
+          be notified when we have more information.
+        </h1>
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello {user.name}</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">Hi {user.name}!</h1>
+
+        <p className="text-md text-gray-800">
+          Welcome to your first round interview for The Residency! This will be
+          a 15 minute call with <span className="font-bold">Res</span>, our
+          residental AI interviewer. The call will take ~15 minutes. MORE
+          STUFF...
+        </p>
+        <Button size="sm">Start Interview</Button>
       </div>
     </div>
   );
