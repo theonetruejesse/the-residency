a3332c0b1c8a27b0eaff5e31f28d4f450c8970dc
dd <the.jesse.dev@gmail.com>
2025-04-27 14:48:38 -0700

    react compiler

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@residency/ui", "@residency/api"],
+  experimental: {
+    reactCompiler: true,
+  },
   images: {
     remotePatterns: [
       {
